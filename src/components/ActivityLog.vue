2e9ecbc58e9ee3b1be24254577fe63f6830d72b1
Chesten <43685339+Chesten@users.noreply.github.com>
2021-05-22 23:19:07 -0400

    update
    activityLog updated

--- src/components/ActivityLog.vue
+++ src/components/ActivityLog.vue
@@ -6,20 +6,6 @@
                 <li><b>Selecting a country from the dropDown will change the 
                  name at the title of "Trends in..", but you need to click on what
                 kind of data you want for it to display in the table and the graph </b></li>
-                <li>Not implemented due to not knowing how to send data 
-                back and forth between all the components.</li>
-                <li>I can bind data from app to the components, but 
-                was having trouble going the other way.</li>
-                <li>In this Component, but also with the "countryStatus" 
-                Component, I was having an issue trying to make a function
-                call either through props, or having a "watcher" on the 
-                data so it would trigger the function when the prop changes.  </li>
-                <li>I was also confused as to why we needed to have any DB integration
-                when the API would act like a "db" in a sense that each call would result 
-                in the data that we want, and can be saved localy, and manipulated, and since
-                there's no "sessionID" issues going on, when the user wants data, they can just get it
-                directly from the API, instead of storing it elsewhere </li>
-                
             </ul>
  
         </div>
