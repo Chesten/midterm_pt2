b4ae5d151f69c24d6150256272d517414519f455
Chesten <43685339+Chesten@users.noreply.github.com>
2021-05-22 23:26:09 -0400

    layout changes
    layout changes

--- src/components/ActivityLog.vue
+++ src/components/ActivityLog.vue
@@ -4,16 +4,18 @@
         <div id="list">
             <ul >
                 <li><b>Selecting a country from the dropDown will change the 
-                 name at the title of "Trends in..", but you need to click on what
-                kind of data you want for it to display in the table and the graph </b></li>
+                 name at the title of "Trends in.." </b></li>
+                 <li>You need to click on what kind of data you want for 
+                 it to display in the table and the graph </li>
             </ul>
- 
         </div>
     </div>
 </template>
 
 <style>
-
+#list{
+    max-width: 25em;
+}
 </style>
 
 <script lang="ts">
